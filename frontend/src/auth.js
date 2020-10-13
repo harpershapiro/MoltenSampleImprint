bc805eaa3f266b902147e15d18ebc6e20707362c
harper.shapiro <harper.shapiro@gmail.com>
2020-10-13 12:42:10 -0500

    removing frontend to replace with submodule

--- frontend/src/auth.js
+++
@@ -1,7 +0,0 @@
-export const isAuth = user => user!=undefined;
-
-export const hasRole = (user, roles) =>
-    roles.some(role => user.user_roles.includes(role));
-
-export const loginUser = (user, app) =>
-    app.setState({user: user});
