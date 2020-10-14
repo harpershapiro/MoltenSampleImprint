ab11cb90b2f6d3073147f603c2bcb0c64f32bc42
harper.shapiro <harper.shapiro@gmail.com>
2020-10-13 19:58:42 -0500

    removing backend to replace with submodule

--- backend/routes/Api.js
+++
@@ -1,9 +0,0 @@
-const apiRouter = require('express').Router(); 
-
-apiRouter.use('/submissions', require('./Submission.js'));
-apiRouter.use('/posts', require('./Post.js'));
-apiRouter.use('/users/', require('./Auth.js'));
-apiRouter.use('/files', require('./Files.js'));
-
-
-module.exports = apiRouter;
